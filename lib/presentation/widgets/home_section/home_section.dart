4af97c443631692dbf79b1825b0bf3cf36af5f54
Eric Lu <revenge.ez.123@gmail.com>
2025-11-08 21:37:34 -0500

    added image to code

--- lib/presentation/widgets/home_section/home_section.dart
+++ lib/presentation/widgets/home_section/home_section.dart
@@ -9,54 +9,62 @@ class HomeSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children:
-          [
-                Text(
-                  "Hi, my name is",
-                  style: GoogleFonts.mPlus1(fontSize: 25, color: Colors.grey),
-                ),
-                //const SizedBox(height: 3),
-                Text(
-                  "Chenyu Lu",
-                  style: GoogleFonts.mPlus1(
-                    fontSize: 60,
-                    fontWeight: FontWeight.bold,
-                    color: const Color.fromARGB(255, 255, 255, 255),
-                  ),
-                ),
-                const SizedBox(height: 12),
-                Text(
-                  "I am a Grade 11 IB Student interested in Computer Science",
-                  style: GoogleFonts.mPlus1(fontSize: 22),
-                ),
-
-                const SizedBox(height: 40),
-
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 32,
-                      vertical: 20,
+    return Row(
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children:
+              [
+                    Text(
+                      "Hi, my name is",
+                      style: GoogleFonts.mPlus1(
+                        fontSize: 25,
+                        color: Colors.grey,
+                      ),
                     ),
-                    backgroundColor: Colors.deepPurple,
-                    foregroundColor: Colors.white,
-                  ),
-                  child: Text(
-                    "View My Projects",
-                    style: GoogleFonts.mPlus1(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
+                    //const SizedBox(height: 3),
+                    Text(
+                      "Chenyu Lu",
+                      style: GoogleFonts.mPlus1(
+                        fontSize: 60,
+                        fontWeight: FontWeight.bold,
+                        color: const Color.fromARGB(255, 255, 255, 255),
+                      ),
                     ),
-                  ),
-                ),
-              ]
-              .animate(interval: 200.ms)
-              .fadeIn(duration: 600.ms, delay: 300.ms)
-              .slideY(begin: 0.5, end: 0.0),
+                    const SizedBox(height: 12),
+                    Text(
+                      "I am a Grade 11 IB Student interested in Computer Science",
+                      style: GoogleFonts.mPlus1(fontSize: 22),
+                    ),
+
+                    const SizedBox(height: 40),
+
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 32,
+                          vertical: 20,
+                        ),
+                        backgroundColor: Colors.deepPurple,
+                        foregroundColor: Colors.white,
+                      ),
+                      child: Text(
+                        "View My Projects",
+                        style: GoogleFonts.mPlus1(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ]
+                  .animate(interval: 200.ms)
+                  .fadeIn(duration: 600.ms, delay: 300.ms)
+                  .slideY(begin: 0.5, end: 0.0),
+        ),
+        Image.asset('images/app_grid.png'),
+      ],
     );
   }
 }
