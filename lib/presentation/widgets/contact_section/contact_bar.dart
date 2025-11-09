aea8f255ea7e8f17f9b5bb66f529622203bd98ec
Eric Lu <revenge.ez.123@gmail.com>
2025-11-09 11:13:41 -0500

    added url redirect for contacts

--- lib/presentation/widgets/contact_section/contact_bar.dart
+++ lib/presentation/widgets/contact_section/contact_bar.dart
@@ -1,34 +1,78 @@
 import 'package:flutter/material.dart';
-import "package:simple_icons/simple_icons.dart";
+import 'package:url_launcher/url_launcher.dart';
 
 class ContactBar extends StatelessWidget {
   const ContactBar({super.key});
 
   @override
+  //can reuse this function for other widgets
+  void openLink(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      throw 'error, cant launch url $url';
+    }
+  }
+
+  void sendEmail() async {
+    final Uri emailUri = Uri(scheme: 'mailto', path: 'eric.luchenyu@gmail.com');
+
+    if (await canLaunchUrl(emailUri)) {
+      await launchUrl(emailUri);
+    } else {
+      throw 'Could not launch email';
+    }
+  }
+
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        SizedBox(
-          height: 35,
-          width: 35,
-          child: Image.asset('images/contact_icons/1.png'),
+        MouseRegion(
+          cursor: SystemMouseCursors.click,
+          child: GestureDetector(
+            onTap: () {
+              openLink('https://github.com/HisMonDon');
+            },
+            child: SizedBox(
+              height: 35,
+              width: 35,
+              child: Image.asset('images/contact_icons/1.png'),
+            ),
+          ),
         ),
         SizedBox(width: 5),
         Container(width: 1, height: 30, color: Colors.grey),
 
         SizedBox(width: 5),
-        SizedBox(
-          height: 30,
-          width: 30,
-          child: Image.asset('images/contact_icons/2.png'),
+        MouseRegion(
+          cursor: SystemMouseCursors.click,
+          child: GestureDetector(
+            onTap: () {
+              openLink('https://www.linkedin.com/in/chenyu-lu-53b09938a/');
+            },
+            child: SizedBox(
+              height: 30,
+              width: 30,
+              child: Image.asset('images/contact_icons/2.png'),
+            ),
+          ),
         ),
 
         SizedBox(width: 5),
         Container(width: 1, height: 30, color: Colors.grey),
         SizedBox(width: 5),
-        Icon(Icons.email_outlined, color: Colors.blue, size: 30.0),
+        MouseRegion(
+          cursor: SystemMouseCursors.click,
+          child: GestureDetector(
+            onTap: () {
+              sendEmail();
+            },
+            child: Icon(Icons.email_outlined, color: Colors.blue, size: 30.0),
+          ),
+        ),
       ],
     );
   }
