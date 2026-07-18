4e4db61c8ce7a338817c3645e0eae57a8114d893
HisMonDon <revenge.ez.123@gmail.com>
2026-07-18 14:16:14 -0400

    removed duplicate footer in credits

--- lib/presentation/widgets/credits_section/credits_section.dart
+++ lib/presentation/widgets/credits_section/credits_section.dart
@@ -109,7 +109,6 @@ class CreditsSection extends StatelessWidget {
               padding: const EdgeInsets.symmetric(
                 horizontal: horizontalPadding,
               ),
-              child: const Footer(),
             ),
           ],
         );
